e193ffcca110b65a9b60f6a834c0c3d4284fbea1
krcma96 <krcma96@gmail.com>
2022-05-26 20:18:56 +0200

    Base logic done

--- orderTracker.js
+++ orderTracker.js
@@ -11,12 +11,19 @@ class User
 {
     constructor(name) {
       this.name = name;
-      this.orderHistory = [];
+      this.orderHistory = {};
     }
 
     AddPurchasedItem(item)
     {
-        this.orderHistory.push(item); 
+        if (this.orderHistory[item])
+        {
+            this.orderHistory[item]++;
+        }
+        else
+        {
+            this.orderHistory[item] = 1;
+        }
     }
 }
 
@@ -65,18 +72,28 @@ function AddNewItem(name, price)
     SaveToLocalStorage();
 }
 
-function PurchaseRound(itemName, userList)
+function PurchaseRound(item, userList)
 {
-    var userNamesList = [];
-    userList.forEach(user => 
-        {
-            user.AddPurchasedItem(itemName);
-            userNamesList.push(user.name);
-        });
+    if (!item || users.length == 0)
+    {
+        return;
+    }
 
-    data.orderHistory.push({itemName, userNamesList});  
+    let itemName = item.name;
+    let userNamesList = [];
 
-    //Refresh order history UI
+    for (user of userList)
+    {
+        user.AddPurchasedItem(itemName);
+        userNamesList.push(user.name);
+    }
+    
+    itemName = item.name;
+    purchaseTime = Date.now();
+    orderHistoryEntry = {itemName, userNamesList, purchaseTime};
+    data.orderHistory.push(orderHistoryEntry);  
+    
+    RefreshStatisticsUI();
     SaveToLocalStorage(); 
 }
 
@@ -147,6 +164,210 @@ function AddUsersSelectUI(user)
     document.getElementById("usersDiv").appendChild(newDiv);
 }
 
+function GetUserByName(name)
+{
+    for (let i=0; i<data.users.length; i++)
+    {
+        if (name.toLowerCase() == data.users[i].name.toLowerCase())
+        {
+            return data.users[i];
+        }
+    }
+}
+
+function GetItemByName(name)
+{
+    for (let i=0; i<data.availableItems.length; i++)
+    {
+        if (name.toLowerCase() == data.availableItems[i].name.toLowerCase())
+        {
+            return data.availableItems[i];
+        }
+    }
+}
+
+function GetSelectedItem()
+{
+    var radioButtons = document.getElementsByName('selectedItem');
+    for (var radioButton of radioButtons) {
+        if (radioButton.checked)
+        {
+            return GetItemByName(radioButton.value);
+        }
+    }
+    return null;
+}
+
+function GetSelectedUsers()
+{
+    let selectedUsers = [];
+    
+    var checkboxes = document.getElementsByName('selectedUsers');
+    for (var checkbox of checkboxes) {
+        if (checkbox.checked)
+        {
+            selectedUsers.push(GetUserByName(checkbox.value));
+        }
+    }
+    return selectedUsers;
+}
+
+function SelectAllUsers()
+{
+    let value = document.getElementById("selectAll").checked;
+
+    var checkboxes = document.getElementsByName('selectedUsers');
+    for (var checkbox of checkboxes) {
+        checkbox.checked = value;
+    }
+}
+
+function AddRoundFromSelected()
+{
+    item = GetSelectedItem();
+    users = GetSelectedUsers();
+    
+    PurchaseRound(item, users);
+    DeselectAll();
+}
+
+function DeselectAll()
+{
+    var users = Array.from(document.getElementsByName('selectedUsers'));
+    var items = Array.from(document.getElementsByName('selectedUsers'));
+    var combined = users.concat(items);
+    for (object of combined)
+    {
+        object.checked = false;
+    }
+}
+
+function CreateRow(columns)
+{
+    const newRow = document.createElement("tr");
+
+    for (column of columns)
+    {
+        newRow.appendChild(CreateColumn(column));
+    }
+
+    return newRow;
+}
+
+function RefreshOrderHistoryUI()
+{
+    RemoveAllChildren(document.getElementById("orderHistoryDiv"));
+
+    let orderHistoryTable = document.getElementById("orderHistoryDiv");
+    orderHistoryTable.appendChild(CreateRow(["Item","Number", "Users ordered", "Time"]));
+
+    for (order of data.orderHistory)
+    {
+        orderHistoryTable.appendChild(
+            CreateRow(
+            [
+                order.itemName,
+                order.userNamesList.length,
+                order.userNamesList,
+                (new Date(order.purchaseTime)).toLocaleTimeString()
+            ]
+            ))
+    }
+}
+
+function CreateColumn(text)
+{
+    let column = document.createElement("th");
+    column.innerText = text;
+    return column;
+}
+
+function AddSummaryByItemUI(itemName, number, price, totalCost)
+{
+    newRow.appendChild(CreateColumn(itemName));
+    newRow.appendChild(CreateColumn(number));
+    newRow.appendChild(CreateColumn(price));
+    newRow.appendChild(CreateColumn(totalCost));
+
+    document.getElementById("summaryByItem").appendChild(newRow);
+}
+
+function CreateTableSummaryByItem(dict)
+{
+    let table = document.createElement("table");
+    table.appendChild(CreateRow(["Item", "Number", "Price", "Total"]));
+    let total = 0;
+
+    for (const [key, value] of Object.entries(dict)) {
+        let itemName = key;
+        let number = value;
+        let price = GetItemByName(itemName).price;
+        let totalByItem = (number*price);
+
+        table.appendChild(CreateRow([itemName, number, price+" RSD", totalByItem+" RSD"]));
+        total += totalByItem;
+    }
+
+    let totalRow = CreateRow(["", "", ""]);
+    let lastColumn = CreateColumn(total +" RSD");
+    lastColumn.style = "border:5px solid black !important;";
+    totalRow.appendChild(lastColumn);
+
+    table.appendChild(totalRow);
+
+    return table;
+}
+
+function RefreshSummaryByItemUI()
+{
+    let div = document.getElementById("summaryByItem");
+    RemoveAllChildren(div);
+
+    let itemDict = {};
+
+    for (order of data.orderHistory)
+    {
+        if (itemDict[order.itemName])
+        {
+            itemDict[order.itemName] += order.userNamesList.length;
+        }
+        else
+        {
+            itemDict[order.itemName] = order.userNamesList.length;
+        }
+    }
+
+    div.appendChild(CreateTableSummaryByItem(itemDict));
+}
+
+function CreateParagraph(text)
+{
+    let paragraph = document.createElement("p");
+    paragraph.innerText = text;
+    return paragraph;
+}
+
+function RefreshSummaryByUsersUI()
+{
+    let div = document.getElementById("summaryByUser");
+    RemoveAllChildren(div);
+
+    for (user of data.users)
+    {
+        div.appendChild(CreateParagraph(user.name));
+
+        div.appendChild(CreateTableSummaryByItem(user.orderHistory));
+    }
+}
+
+
+function RefreshStatisticsUI()
+{
+    RefreshOrderHistoryUI();
+    RefreshSummaryByItemUI();
+    RefreshSummaryByUsersUI();
+}
+
 window.addEventListener('load', (event) => {
 
     if (localStorage.data === undefined)
@@ -156,9 +377,20 @@ window.addEventListener('load', (event) => {
     else
     {
         data = JSON.parse(localStorage.data);
+
+        for (user of data.users)
+        {
+            Object.setPrototypeOf(user, User.prototype);
+        }
+        
+        for (item of data.availableItems)
+        {
+            Object.setPrototypeOf(item, Item.prototype);
+        }
     }
 
     RefreshItemsUI();
-    
     RefreshUsersSelectUI();
+
+    RefreshStatisticsUI();
 });
